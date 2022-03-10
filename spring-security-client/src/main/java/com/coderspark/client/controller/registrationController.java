d87da051181a61be8fbec0e8f4bed6afb8ef8ce0
Sahil Girme <sahilgirme30@gmail.com>
2022-03-10 23:17:47 +0530

    verifying verification token

--- spring-security-client/src/main/java/com/coderspark/client/controller/registrationController.java
+++ spring-security-client/src/main/java/com/coderspark/client/controller/registrationController.java
@@ -6,9 +6,7 @@ import com.coderspark.client.model.UserModel;
 import com.coderspark.client.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -29,6 +27,17 @@ public class registrationController {
         ));
         return "Success";
     }
+
+    @GetMapping("/verifyRegistration")
+    public String verifyRegistration(@RequestParam("token") String token){
+        String result = userService.validateVerificationToken(token);
+        if(result.equalsIgnoreCase("valid")){
+            return "User Verified Successfully";
+        }
+        return "Bad User";
+
+    }
+
 
     private String applicationUrl(HttpServletRequest request) {
         return "http://"+
--- spring-security-client/src/main/java/com/coderspark/client/service/UserServiceImpl.java
+++ spring-security-client/src/main/java/com/coderspark/client/service/UserServiceImpl.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Calendar;
+
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -36,8 +38,29 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void saveVerificationTokenForUser(String token, User user) {
-        VerificationToken verificationToken = new VerificationToken(user,token);
+        VerificationToken verificationToken = new VerificationToken(user, token);
 
         verificationTokenRepository.save(verificationToken);
+    }
+
+    @Override
+    public String validateVerificationToken(String token) {
+        VerificationToken verificationToken = verificationTokenRepository.findByToken(token);
+
+        if (verificationToken == null) {
+            return "invalid";
+        }
+        User user = verificationToken.getUser();
+        Calendar cal = Calendar.getInstance();
+
+        if (verificationToken.getExpirationTime().getTime()
+                - cal.getTime().getTime() <= 0){
+            verificationTokenRepository.delete(verificationToken);
+            return "expired";
+        }
+
+        user.setEnabled(true);
+        userRepository.save(user);
+        return "Valid";
     }
 }
